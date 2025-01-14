a6a4767116d1fb830e308530aa36f96122de7cd9
philliephanatic <nolan.r.young@gmail.com>
2025-01-13 21:34:15 -0500

    update dice images, background colors, text colors, add button (CSS only), grid layout

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function diceRoll () {
     let randomNumber1 = Math.floor(Math.random() * 6) + 1;
     let randomNumber2 = Math.floor(Math.random() * 6) + 1;
     
-    let diceImagePlayer1 = document.querySelector("img.img1").setAttribute("src",`/images/dice${randomNumber1}.png`);
-    let diceImagePlayer2 = document.querySelector("img.img2").setAttribute("src",`/images/dice${randomNumber2}.png`);
+    let diceImagePlayer1 = document.querySelector("img.img1").setAttribute("src",`/images/dice${randomNumber1}.svg`);
+    let diceImagePlayer2 = document.querySelector("img.img2").setAttribute("src",`/images/dice${randomNumber2}.svg`);
 
     if (randomNumber1 > randomNumber2) {
             document.querySelector("h1").innerHTML = "Player 1 wins!";
