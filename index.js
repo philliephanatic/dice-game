f545abda57e2281830a89191fa1a2eb3990c7af7
philliephanatic <nolan.r.young@gmail.com>
2025-01-15 18:20:48 -0500

    add score counter JS snippet,temporary black boarders around Score

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let player1Score = 0;
+let player2Score = 0;
+
 function diceRoll () {
     let randomNumber1 = Math.floor(Math.random() * 6) + 1;
     let randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -7,11 +10,14 @@ function diceRoll () {
 
     if (randomNumber1 > randomNumber2) {
             document.querySelector("h1").innerHTML = "Player 1 wins!";
+            player1Score++;
+            document.querySelector("p.score-counter-1").innerHTML = player1Score;
     } else if (randomNumber1 < randomNumber2) {
             document.querySelector("h1").innerHTML = "Player 2 wins!";
+            player2Score++;
+            document.querySelector("p.score-counter-2").innerHTML = player2Score;
     } else
             document.querySelector("h1").innerHTML = "Draw!";
 }
 
-document.querySelector("button").addEventListener("click",
-        diceRoll);
+document.querySelector("button").addEventListener("click", diceRoll);
